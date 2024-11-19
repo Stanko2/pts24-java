7d569dc827f2b0ac38c3902cdf7ffd7703329165
Stanko <stanko@bezak.net>
2024-11-19 13:37:50 +0100

    CI fix

--- src/main/java/sk/uniba/fmph/dcs/game_board/BuildingTile.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/BuildingTile.java
@@ -14,13 +14,13 @@ public class BuildingTile implements InterfaceFigureLocationInternal {
     private final ArrayList<PlayerOrder> figures;
 
 
-    public BuildingTile(Building building) {
+    public BuildingTile(final Building building) {
         this.building = building;
         figures = new ArrayList<>();
     }
 
     @Override
-    public boolean placeFigures(Player player, int figureCount) {
+    public boolean placeFigures(final Player player, final int figureCount) {
         var action = tryToPlaceFigures(player, figureCount);
         if (action == HasAction.WAITING_FOR_PLAYER_ACTION) {
             figures.add(player.playerOrder());
@@ -31,7 +31,7 @@ public class BuildingTile implements InterfaceFigureLocationInternal {
     }
 
     @Override
-    public HasAction tryToPlaceFigures(Player player, int count) {
+    public HasAction tryToPlaceFigures(final Player player, final int count) {
         if (count > 1)
             return HasAction.NO_ACTION_POSSIBLE;
         if (figures.contains(player.playerOrder())) {
@@ -41,10 +41,11 @@ public class BuildingTile implements InterfaceFigureLocationInternal {
     }
 
     @Override
-    public ActionResult makeAction(Player player, Effect[] inputResources, Effect[] outputResources) {
+    public ActionResult makeAction(final Player player, final Effect[] inputResources, final Effect[] outputResources) {
         OptionalInt points = building.build(List.of(inputResources));
-        if (tryToMakeAction(player) == HasAction.NO_ACTION_POSSIBLE)
+        if (tryToMakeAction(player) == HasAction.NO_ACTION_POSSIBLE) {
             return ActionResult.FAILURE;
+        }
         if (points.isEmpty()) {
             return ActionResult.FAILURE;
         }
@@ -56,13 +57,13 @@ public class BuildingTile implements InterfaceFigureLocationInternal {
     }
 
     @Override
-    public boolean skipAction(Player player) {
+    public boolean skipAction(final Player player) {
         return false;
     }
 
     @Override
-    public HasAction tryToMakeAction(Player player) {
-        if(figures.contains(player.playerOrder())) {
+    public HasAction tryToMakeAction(final Player player) {
+        if (figures.contains(player.playerOrder())) {
             return HasAction.WAITING_FOR_PLAYER_ACTION;
         }
         return HasAction.NO_ACTION_POSSIBLE;
